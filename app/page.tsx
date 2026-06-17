cc5e41aabe643f4bf94f8655d8b908a831b61a52
gaurav0973 <gauravKmaurya09@gmail.com>
2026-06-17 22:27:50 +0530

    aauthentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+'use client'
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import Image from "next/image";
+import { authClient } from "../lib/auth-client";
+import { UserMenuWithSession } from "@/features/auth/components/user-menu";
 
 export default function Home() {
+  const { data } = authClient.useSession();
+  console.log("Session data:", data);
+  console.log("Session user:", data?.user);
+  console.log("Session user email:", data?.session);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <UserMenuWithSession variant="compact"/>
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
---
+++ lib/auth.ts
@@ -0,0 +1,19 @@
+import { prisma } from './db';
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from 'better-auth/next-js';
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    }),
+    socialProviders: {
+        github: {
+        clientId: process.env.GITHUB_CLIENT_ID as string,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
+    },
+
+    // allows you to use cookies for authentication in Next.js
+    plugins: [nextCookies()],
+});
