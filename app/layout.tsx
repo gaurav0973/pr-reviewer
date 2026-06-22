f48127c98932666f1292e991719abd5c1cf63e02
gaurav0973 <gauravKmaurya09@gmail.com>
2026-06-22 11:49:01 +0530

    github sync and RaG

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/provider/theme-provider";
 import { QueryProvider } from "@/components/provider/query-provider";
+import { Toaster } from "@/components/ui/sonner";
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -38,6 +41,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Toaster />
           </ThemeProvider>
         </QueryProvider>
       </body>
