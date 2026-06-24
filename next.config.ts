db092c289d1ef9921c0bc86b3db0351ee37ae932
gaurav0973 <gauravKmaurya09@gmail.com>
2026-06-24 11:46:40 +0530

    Production: auth url change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["upload-purging-exile.ngrok-free.dev"],
+  allowedDevOrigins: [
+    "pr-reviewer-ochre.vercel.app",
+    "upload-purging-exile.ngrok-free.dev",
+  ],
 };
 
 export default nextConfig;
